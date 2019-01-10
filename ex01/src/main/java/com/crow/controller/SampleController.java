430f0a0911ec539833560e3636efc11a40c05f39
oneso123456789 <40660368+oneso123456789@users.noreply.github.com>
2019-01-11 06:24:16 +0900

    2019-01-11 154p 6.6 전까지함 더 열심히 노력하자 ㅠㅠ

--- ex01/src/main/java/com/crow/controller/SampleController.java
+++ ex01/src/main/java/com/crow/controller/SampleController.java
@@ -6,14 +6,20 @@ import java.util.Arrays;
 import java.util.Date;
 
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.crow.domain.SampleDTO;
 import com.crow.domain.SampleDTOList;
@@ -113,6 +119,45 @@ public class SampleController {
 		log.info("/ex05..........");
 	}
 	
+	@GetMapping("/ex06")
+	public @ResponseBody SampleDTO ex06() {
+		
+		log.info("/ex06..........");
+		SampleDTO dto = new SampleDTO();
+		dto.setAge(23);
+		dto.setName("Crow");
+		
+		return dto;
+	}
 	
+	@GetMapping("/ex07")
+	public ResponseEntity<String> ex07(){
+		
+		log.info("/ex07..........");
+// 		{"name": "크로우"}
+		String msg = "{\"name\": \"크로우\"}";
+		
+		HttpHeaders header = new HttpHeaders();
+		header.add("Content-Type", "application/json;charset=UTF-8");
+		
+		return new ResponseEntity<>(msg, header, HttpStatus.OK);
+	}
+	
+	@GetMapping("/exUpload")
+	public void exUpload() {
+		
+		log.info("/exUpload..........");
+	}
+	
+	@PostMapping("/exUploadPost")
+	public void exUploadPost(ArrayList<MultipartFile> files) {
+		
+		files.forEach(file -> {
+			log.info("------------------------------");
+			log.info("name:" + file.getOriginalFilename());
+			log.info("size:" + file.getSize());
+			
+		});
+	}
 
 }
